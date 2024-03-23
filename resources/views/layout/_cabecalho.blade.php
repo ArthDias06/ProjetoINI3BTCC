db4fdf1a2463c106ed704fc22fa7c2d901db24ce
Arthur de Castro Dias <arthurcastrodias@gmail.com>
2024-03-23 07:52:04 -0300

    Melhoria

--- resources/views/layout/_cabecalho.blade.php
+++ resources/views/layout/_cabecalho.blade.php
@@ -1,6 +1,6 @@
 <html>
     <header><title>@yield('titulo')</title></header>
-    <body>
-        <div style="background-color: green">
-            <img src="https://thumbs.dreamstime.com/z/leves-7818111.jpg" width="50px" height="50px" alt="Logo"><hr>
+    <body style="margin: 0; padding: 0;">
+        <div style="background-color: green;">
+            <img src="https://thumbs.dreamstime.com/z/leves-7818111.jpg" width="50px" height="50px" alt="Logo" style="float: left;"><center><h1 style="color: red;">Leaves</h1></center><hr>
         </div>
