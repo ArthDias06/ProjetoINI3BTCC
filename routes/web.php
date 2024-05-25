6471dd14df3dc8adff537f539f3e1799652faf83
ArthDias06 <arthurcastrodias@gmail.com>
2024-05-25 09:10:24 -0300

    td mudado

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('admin/cursos',['as'=>'admin.cursos',
+Route::get('/',['as'=>'admin.cursos',
 'uses'=>'App\Http\Controllers\Admin\CursoController@index']);
 
 Route::get('admin/cursos/adicionar',['as'=>'admin.cursos.adicionar',
